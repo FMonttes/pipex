2dfb899c0db90e61bd80b90e878a30e9c113aa87
FMontes <felipe.monttes100@gmail.com>
2023-12-24 15:50:04 -0300

    comitando apenas com um codigo com fork

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: felipe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 20:44:52 by felipe            #+#    #+#             */
-/*   Updated: 2023/12/24 15:26:31 by felipe           ###   ########.fr       */
+/*   Updated: 2023/12/24 15:49:57 by felipe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,13 +40,63 @@ int main(int ac, char **av, char **envp)
 	else
 	{
 		wait(NULL);
-		printf("PID do processo pai: %d\n", getpid());
-		
-		//char *cmd = "/user/bin/git";
-		char *args[] = {"git", "commit", "-m", "teste",  NULL};
+		printf("\nestou no processo pai\n");
 
-		execve("/usr/bin/git" , args, envp);
+		pid_t pid_commit = fork();
+		if (pid_commit == -1)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid_commit == 0)
+		{
+			printf("\nestou no processo filho do commit\n");
 
-		printf ("processo pai termina aqui\n");	
-	}
+			char *args[] = {"git", "commit", "-m", "comitando apenas com um codigo com fork", NULL};
+			execve("/usr/bin/git", args, envp);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(NULL);
+			printf("\n estou em outro processo pai\n");
+
+			pid_t pid_push = fork();
+			
+			if (pid_push == -1)
+			{
+				perror("fork");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid_push == 0)
+			{
+				printf("\n estou em outro processo filho\n");
+
+				char *args[] = {"git", "push", NULL};
+				execve("/usr/bin/git", args, envp);
+				perror("execve");
+				exit(EXIT_FAILURE);
+			}
+			else
+			{
+				wait(NULL);
+				printf("TODOS OS PROCESSOS TERMINA AQUI");
+			}
+		}
+	}	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
